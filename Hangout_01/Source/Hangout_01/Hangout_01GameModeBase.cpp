5539727a03d4daa00627da4d50cecf3dfce4c6ff
720bit <49247495+720bit@users.noreply.github.com>
2022-11-30 17:44:38 -0600

    Initial commit

---
+++ Hangout_01/Source/Hangout_01/Hangout_01GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Hangout_01GameModeBase.h"
+
---
+++ Hangout_01/Source/Hangout_01/Hangout_01GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Hangout_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANGOUT_01_API AHangout_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
